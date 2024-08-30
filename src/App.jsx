ea82aad56ddeff0d565ff05f108c7e433bcc5ef1
Vectari <mateusz.majer996@gmail.com>
2024-08-30 23:41:50 +0200

    fix: another dependenciesfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Paper from "@mui/material/Paper";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from "@devexpress/dx-react-scheduler-material-ui";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const currentDate = "2018-11-01";
+  const schedulerData = [
+    {
+      startDate: "2018-11-01T09:45",
+      endDate: "2018-11-01T11:00",
+      title: "Meeting",
+    },
+    {
+      startDate: "2018-11-01T12:00",
+      endDate: "2018-11-01T13:30",
+      title: "Go to a gym",
+    },
+  ];
 
   return (
     <>
+      <Paper>
+        <Scheduler data={schedulerData}>
+          <ViewState currentDate={currentDate} />
+          <DayView startDayHour={9} endDayHour={14} />
+          <Appointments />
+        </Scheduler>
+      </Paper>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,7 +58,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
