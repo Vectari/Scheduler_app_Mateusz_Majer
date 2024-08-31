e284c7c40f8a56d32021561cf4e260b28e412bb7
Vectari <mateusz.majer996@gmail.com>
2024-08-31 11:42:00 +0200

    feat: add polish language

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "devextreme/dist/css/dx.light.css";
-import { appointments } from "./data.js";
 import { Scheduler } from "devextreme-react/scheduler";
+import { locale, loadMessages } from "devextreme/localization";
+import plMessages from "devextreme/localization/messages/pl.json";
+import { appointments } from "./data.js";
 
 function App() {
+  locale("pl");
+  loadMessages(plMessages);
+  locale(navigator.locale);
+
   return (
     <>
       <h1>Hello</h1>
