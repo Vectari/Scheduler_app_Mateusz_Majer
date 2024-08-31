7d8d2c7cfcc9ff044044c5f097aa1751e13a5e40
Vectari <mateusz.majer996@gmail.com>
2024-08-31 11:29:38 +0200

    feat: add basic scheduler

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import "devextreme/dist/css/dx.light.css";
+import { appointments } from "./data.js";
+import { Scheduler } from "devextreme-react/scheduler";
+
 function App() {
   return (
     <>
       <h1>Hello</h1>
+      <Scheduler
+        dataSource={appointments}
+        textExpr="title"
+        allDayExpr="dayLong"
+        recurrenceRuleExpr="recurrence"
+      ></Scheduler>
     </>
   );
 }
