412b4b2ef0d795ce3487938526acf38bf04d4b90
Vectari <mateusz.majer996@gmail.com>
2024-08-31 12:14:45 +0200

    style: add simple style to scheduler

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,28 @@ function App() {
   loadMessages(plMessages);
   locale(navigator.locale);
 
+  const views = ["day", "week", "month"];
+
   return (
     <>
-      <h1>Hello</h1>
+      <h2>Scheduler App - Mateusz Majer</h2>
       <Scheduler
+        views={views}
+        useDropDownViewSwitcher={true}
+        defaultCurrentView="month"
+        height={730}
+        width={1700}
+
         dataSource={appointments}
         textExpr="title"
         allDayExpr="dayLong"
         recurrenceRuleExpr="recurrence"
       ></Scheduler>
+      <h3>Instrukcja:</h3>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
     </>
   );
 }
