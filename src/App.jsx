c08ba50fc3e28f1d79e615d42066ddbc7b3153f4
Vectari <mateusz.majer996@gmail.com>
2024-09-02 22:33:19 +0200

    feat: add sensitive data to .env

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,13 @@ function App() {
   const views = ["day", "week", "month"];
 
   const firebaseApp = initializeApp({
-    apiKey: "AIzaSyB_NCNNCPcB14FygD_0yiyaeMrAvujF4y4",
-    authDomain: "schedulerappmateuszmajer.firebaseapp.com",
-    projectId: "schedulerappmateuszmajer",
-    storageBucket: "schedulerappmateuszmajer.appspot.com",
-    messagingSenderId: "167844971416",
-    appId: "1:167844971416:web:1788605f57ff5edac8e1ed",
-    measurementId: "G-YPPBLXVH4K",
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
   });
 
   const firestore = getFirestore(firebaseApp);
