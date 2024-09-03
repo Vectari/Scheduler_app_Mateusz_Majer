59694cdaca98603e1a294aa65574aefe7251a734
Vectari <mateusz.majer996@gmail.com>
2024-09-03 23:23:58 +0200

    style: add simple style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import styled from "styled-components";
 import SchedulerComponent from "./components/SchedulerComponent";
 import InstructionsComponent from "./components/InstructionsComponent";
+
+const StyledSchedulerWrapper = styled.div`
+  display: grid;
+  place-items: center;
+`;
 
 function App() {
   return (
     <>
-      <SchedulerComponent />
+      <StyledSchedulerWrapper>
+        <SchedulerComponent />
+      </StyledSchedulerWrapper>
       <InstructionsComponent />
     </>
   );
