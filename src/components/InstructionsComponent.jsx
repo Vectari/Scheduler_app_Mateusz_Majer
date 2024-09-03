644f44b0f9a32228c1d17440756c75eddd06d393
Vectari <mateusz.majer996@gmail.com>
2024-09-03 23:19:07 +0200

    feat: add simple instruction

--- src/components/InstructionsComponent.jsx
+++ src/components/InstructionsComponent.jsx
@@ -1,12 +1,31 @@
+import styled from "styled-components";
+
+const StyledInstructionsWrapper = styled.div`
+  margin-left: 5rem;
+`;
+
 const InstructionsComponent = () => {
   return (
-    <div>
+    <StyledInstructionsWrapper>
       <h3>Instrukcja:</h3>
       <ul>
-        <li></li>
-        <li></li>
+        <li>
+          <b>Tworzenie spotkania:</b> Dwukrotnie kliknij na wybrany przedział
+          czasowy, aby utworzyć nowe spotkanie. Otworzy się formularz, w którym
+          możesz wprowadzić szczegóły spotkania. Wypełnij wymagane informacje i
+          zapisz.
+        </li>
+        <li>
+          <b>Edycja spotkania:</b> Dwukrotnie kliknij na istniejące spotkanie,
+          aby je edytować. Możesz zaktualizować szczegóły w formularzu, który
+          się pojawi, a następnie zapisać zmiany.
+        </li>
+        <li>
+          <b>Usuwanie spotkania:</b> Kliknij na spotkanie, aby otworzyć
+          szczegóły. Następnie kliknij ikonę kosza, aby usunąć spotkanie.
+        </li>
       </ul>
-    </div>
+    </StyledInstructionsWrapper>
   );
 };
 
